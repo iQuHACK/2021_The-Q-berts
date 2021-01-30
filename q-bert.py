7d0edeef0dedb7e743273c7a93c90af067f79b6d
michaelsantos001 <71531198+michaelsantos001@users.noreply.github.com>
2021-01-30 08:55:00 -0800

    Update q-bert.py

--- q-bert.py
+++ q-bert.py
@@ -1,9 +1,5 @@
 #insert code here
 import numpy as np
-import matplotlib.pyplot as plt
-from matplotlib import pyplot as plt
-from mpl_toolkits.mplot3d import Axes3D
-from matplotlib.patches import FancyArrowPatch
-from mpl_toolkits.mplot3d import proj3d
 import ipywidgets as widgets
 from IPython.display import display
+
