7c7e1f3af2c5c4c96697d60d3db7f58c3a866e2c
jade <jdll2004@gmail.com>
2021-01-30 21:55:24 +0000

    more changes

--- movement.py
+++ movement.py
@@ -8,11 +8,26 @@ player_image = pyglet.resource.image("qbert.png")
 window = pyglet.window.Window()
 from pyglet.window import key
 
+class PhysicalObject(pyglet.sprite.Sprite):
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+
+        self.velocity_x, self.velocity_y = 0.0, 0.0
+
 @window.event
 def on_key_press(symbol, modifiers):
     if symbol == key.LEFT:
-        print("The left arrow key was pressed")
-    else:
-        print('A key was pressed')
+        x1_change = -10
+        y1_change = 0
+    elif symbol == key.RIGHT:
+        x1_change = 10
+        y1_change = 0
+    elif symbol == key.DOWN:
+        y1_change = -10
+        x1_change = 0
+    elif symbol == key.UP:
+        y1_change = 10
+        x1_change = 0
         
 pyglet.app.run()
