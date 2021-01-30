c3d0ced006d7b0dfc454a0c1c0d03c79e58a1415
michaelsantos001 <71531198+michaelsantos001@users.noreply.github.com>
2021-01-30 09:05:37 -0800

    Update field.py

--- field.py
+++ field.py
@@ -1,1 +1,10 @@
-
+import numpy as np
+from numpy import pi
+import ipywidgets as widgets
+from IPython.display import display
+from qutip import Bloch
+import matplotlib as plt
+import time
+from qiskit import QuantumCircuit, QuantumRegister
+circ.h(0)
+boardspace = [[
