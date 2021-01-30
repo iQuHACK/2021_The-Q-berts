b571dd7581576731ffd0f496c58057c2a08bff39
michaelsantos001 <santosm9876@gmail.com>
2021-01-30 22:03:48 +0000

    puuuuuuuush

--- field.py
+++ field.py
@@ -9,15 +9,17 @@ from qiskit import QuantumCircuit
 from qiskit import Aer, execute
 from qiskit.visualization import plot_histogram
 # makes quantum curcuit for the hieght
-blockHeightCurcuit=QuantumCircuit(1,1)
-blockHeightCurcuit.x(0)
-
-blockHeightCurcuit.draw(output='mpl')
-backend_sim = Aer.get_backend('qasm_simulator')
-sim = execute(blockHeightCurcuit, backend_sim, shots=1000)
+blockHeightCircuit=QuantumCircuit(3)
+blockHeightCircuit.h(1)
+blockHeightCircuit.z(1)
+blockHeightCircuit.h(0)
+blockHeightCircuit.h(2)
+backend_sim = Aer.get_backend('statevector_simulator')
+sim = execute(blockHeightCircuit, backend_sim, shots=5000)
 sim_result = sim.result()
-counts = sim_result.get_counts(blockHeightCurcuit)
+counts = sim_result.get_counts(blockHeightCircuit)
 print(counts)
+blockHeightCircuit.measure
 
 boardspace =[[0, 0, 0, 0, 0, 0, 0], 
              [0, 0, 0, 0, 0, 0, 0],
@@ -27,3 +29,10 @@ boardspace =[[0, 0, 0, 0, 0, 0, 0],
              [0, 0, 0, 0, 0, 0, 0],
              [0, 0, 0, 0, 0, 0, 0]]
 
+plot_histogram(counts)
+def x_measurement(blockHeightCircuit,qubit,cbit):
+
+    blockHeightCircuit.measure(qubit, cbit)
+    blockHeightCircuit.h(qubit)
+    return blockHeightCircuit
+print(x_measurement)
