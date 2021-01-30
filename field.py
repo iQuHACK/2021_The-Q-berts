f8a54a8888b9575a86adb00f5b23da1c76096c6d
michaelsantos001 <71531198+michaelsantos001@users.noreply.github.com>
2021-01-30 10:12:42 -0800

    Update field.py

--- field.py
+++ field.py
@@ -5,6 +5,21 @@ from IPython.display import display
 from qutip import Bloch
 import matplotlib as plt
 import time
-from qiskit import QuantumCircuit, QuantumRegister
+from qiskit import QuantumCircuit
+from qiskit import Aer, execute
+# makes quantum curcuit for the hieght
+blockHeightCurcuit=QuantumCurcuit(2,2)
 circ.h(0)
-boardspace = [[]]
+circ.h(1)
+circ.draw(output='mpl')
+backend_sim = Aer.get_backend('')
+#note- insert name of quantum computer we're using in there^
+for j in range(2):
+    qc_output.measure(j,j)
+    
+boardspace =[[0, 0, 0, 0, 0], 
+             [0, 0, 0, 0, 0], 
+             [0, 0, 0, 0, 0], 
+             [0, 0, 0, 0, 0], 
+             [0, 0, 0, 0, 0]]
+
