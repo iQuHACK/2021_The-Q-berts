41ccfc86357c9a252e53507afc2b3a044897bbdc
littwit41 <78159565+littwit41@users.noreply.github.com>
2021-01-30 16:56:35 -0500

    Update sprites.py

--- sprites.py
+++ sprites.py
@@ -1,1 +1,18 @@
+import pyglet
+import random
 
+avatar_image = pyglet.resource.image("qbert")
+
+def center_image(image):
+  image.anchor_x = image.width//2
+  image.anchor_y = image.height//2
+
+center_image(avatar_image)
+
+class Avatar(pyglet.window.Window):
+  def __init__(self):
+    super(Avatar, self).__init__()
+    self.scale=1
+    self.image=pyglet.resource.image("qbert.png")
+    
+    
