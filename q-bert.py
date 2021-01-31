190d39a8728ee2525a8ee63eb6b9351b1ced3e12
littwit41 <78159565+littwit41@users.noreply.github.com>
2021-01-30 20:09:21 -0500

    Update q-bert.py

--- q-bert.py
+++ q-bert.py
@@ -5,5 +5,15 @@ from IPython.display import display
 import Pyglet
 import random
 
+window=pyglet.window.Window()
+
+  
+
+pyglet
 pyglet.resource.path = ["images"]
 pyglet.resource.reindex()
+
+
+
+def on_draw():
+  window.clear()
